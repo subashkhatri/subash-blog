43b9a2b6e8a405e711d7ba75065514f6201979fd
sk <skbro10@gmail.com>
2020-07-12 12:59:32 +0545

    - Add partials - Add destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
  
   resources :articles
 
+  get 'welcome/home'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
+
 end
