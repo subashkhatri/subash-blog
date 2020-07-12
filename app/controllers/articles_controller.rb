40fbf8a4f91b81dd0807912a870ae29c3a93377e
sk <skbro10@gmail.com>
2020-07-12 12:10:03 +0545

    - Completed edit and update method for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,10 @@ class ArticlesController < ApplicationController
 
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
     #  render plain: params[:article].inspect
     @article = Article.new(article_params)     
@@ -18,11 +22,21 @@ class ArticlesController < ApplicationController
         end
     end
 
-    def show
+    def update
         @article = Article.find(params[:id])
-        
-     
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
     end
+
+    def show
+        @article = Article.find(params[:id])       
+    end
+
+    
 
     private
 
